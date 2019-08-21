6d2475ed7f6f65d5492547e711d964b0686510fe
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2019-08-21 16:54:02 +0530

    Update SecondServiceApplication.java

--- second-service/src/main/java/com/openshift/gatewayservice/secondservice/SecondServiceApplication.java
+++ second-service/src/main/java/com/openshift/gatewayservice/secondservice/SecondServiceApplication.java
@@ -1,4 +1,4 @@
-package com.devglan.gatewayservice.secondservice;
+package com.openshift.gatewayservice.secondservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
