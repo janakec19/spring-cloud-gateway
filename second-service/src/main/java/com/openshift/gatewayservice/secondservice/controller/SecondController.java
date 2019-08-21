8fbaaf342caec86979a5a2ad8ba9519e20ae9cba
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2019-08-21 16:54:33 +0530

    Update SecondController.java

--- second-service/src/main/java/com/openshift/gatewayservice/secondservice/controller/SecondController.java
+++ second-service/src/main/java/com/openshift/gatewayservice/secondservice/controller/SecondController.java
@@ -1,4 +1,4 @@
-package com.devglan.gatewayservice.secondservice.controller;
+package com.openshift.gatewayservice.secondservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
