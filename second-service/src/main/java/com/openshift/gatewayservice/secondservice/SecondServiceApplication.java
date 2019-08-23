aa12e053f6604736848e5c857d784af996f8b201
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2019-08-23 16:54:19 +0530

    Update SecondServiceApplication.java

--- second-service/src/main/java/com/openshift/gatewayservice/secondservice/SecondServiceApplication.java
+++ second-service/src/main/java/com/openshift/gatewayservice/secondservice/SecondServiceApplication.java
@@ -2,7 +2,7 @@ package com.openshift.gatewayservice.secondservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SecondServiceApplication {
